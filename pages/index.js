342e86a4035a7d74d019b789b09391e30639d02a
choo121600 <choo121600@gmail.com>
2023-02-23 16:08:36 +0900

    Feat: 메인화면 추가

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,120 @@
 import { PageSEO } from "@/components/SEO";
 import siteMetaData from "@/data/siteMetaData";
 
+import {
+  Devider,
+  Spacer,
+  SubSecColumn,
+  SubSecColumnLeft,
+  SubSecColumnRight,
+  SubSecInner,
+  SubSecInnerIntro,
+  Logo,
+  SubSecLogoList,
+  SubSecLogo,
+  SubColButtonArea,
+} from "@/styles/pagesStyles/MainHomeStyle";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
 export default function Home({ posts }) {
+  const [width, setWidth] = useState(480);
+  const [height, setHeight] = useState(300);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 767) {
+        setWidth(380);
+        setHeight(250);
+      } else if (window.innerWidth < 1024) {
+        setWidth(340);
+        setHeight(210);
+      } else {
+        setWidth(540);
+        setHeight(360);
+      }
+    }
+    window.addEventListener("resize", handleResize);
+    return (_) => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
       <PageSEO
         title={siteMetaData.title}
         description={siteMetaData.description}
       />
+      <SubSecInner>
+        <SubSecInnerIntro>
+          <SubSecColumn>
+            <SubSecColumnLeft>
+              <h1>개발이 외롭다 느껴지면 이곳으로 오세요</h1>
+              <SubColButtonArea>
+                <a href="https://discord.gg/5dKVjN7rvj">
+                  <span>Discord로 함께하기</span>
+                </a>
+              </SubColButtonArea>
+            </SubSecColumnLeft>
+            <SubSecColumnRight>
+              <Image
+                src={"/static/images/main/mainIntro.png"}
+                alt="main intro"
+                width={width}
+                height={height}
+              />
+            </SubSecColumnRight>
+          </SubSecColumn>
+        </SubSecInnerIntro>
+        <Spacer></Spacer>
+        <SubSecLogo>
+          <Devider></Devider>
+          <Spacer></Spacer>
+          <SubSecLogoList>
+            <Logo>
+              <Image
+                src="/static/images/javascript/javascriptLogo.webp"
+                alt="js logo"
+                width={100}
+                height={70}
+              />
+            </Logo>
+            <Logo>
+              <Image
+                src="/static/images/nodejs/nodejsLogo.png"
+                alt="js logo"
+                width={100}
+                height={70}
+              />
+            </Logo>
+            <Logo>
+              <Image
+                src="/static/images/nestjs/nestjsLogo.png"
+                alt="js logo"
+                width={100}
+                height={70}
+              />
+            </Logo>
+            <Logo>
+              <Image
+                src="/static/images/mariadb/mariadbLogo.png"
+                alt="js logo"
+                width={100}
+                height={70}
+              />
+            </Logo>
+            <Logo>
+              <Image
+                src="/static/images/react/reactLogo.png"
+                alt="js logo"
+                width={100}
+                height={70}
+              />
+            </Logo>
+          </SubSecLogoList>
+          <Spacer></Spacer>
+          <Devider></Devider>
+        </SubSecLogo>
+      </SubSecInner>
     </>
   );
 }
---
+++ styles/pagesStyles/MainHomeStyle.js
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+
+export const SubSecInner = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 0 40px;
+`;
+
+export const SubSecInnerIntro = styled.section`
+  padding: 100px 0;
+  width: 100%;
+`;
+
+export const SubSecColumn = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
+  z-index: 2;
+  width: 100%;
+  @media (max-width: 767px) {
+    flex-direction: column;
+  }
+`;
+
+export const SubSecColumnLeft = styled.div`
+  position: relative;
+  max-width: 100%;
+  width: 100%;
+  word-break: keep-all;
+  h1 {
+    font-size: calc(42px + 1.5vw);
+    font-weight: 600;
+  }
+  @media (max-width: 767px) {
+    padding-bottom: 45px;
+    padding-top: 45px;
+  }
+`;
+
+export const SubSecColumnRight = styled.div`
+  position: relative;
+  max-width: 100%;
+  width: 100%;
+  word-break: keep-all;
+  @media (min-width: 767px) {
+    padding-left: 90px;
+  }
+`;
+
+export const SubColButtonArea = styled.div`
+  margin-top: 30px;
+  height: 48px;
+  a {
+    padding: 10px 20px;
+    border-radius: 20px;
+    align-items: center;
+    color: #fff;
+    display: inline-flex;
+    justify-content: center;
+    background-color: #6210cc;
+
+    &:hover {
+      background-color: #4a0e9f;
+    }
+  }
+`;
+
+export const Spacer = styled.div`
+  height: 30px;
+`;
+
+export const Devider = styled.div`
+  width: 100%;
+  height: 1px;
+
+  @media (prefers-color-scheme: dark) {
+    background-color: #eaeaea;
+  }
+  @media (prefers-color-scheme: light) {
+    background-color: #e0e0e0;
+  }
+`;
+
+export const SubSecLogo = styled.div`
+  width: 100%;
+`;
+
+export const SubSecLogoList = styled.section`
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+`;
+
+export const Logo = styled.div`
+  margin: 0 20px;
+  text-align: center;
+  width: calc(16% - 34px);
+  @media (max-width: 1024px) {
+    width: calc(33.3333% - 40px);
+  }
+  @media (max-width: 767px) {
+    width: calc(50% - 40px);
+  }
+`;
