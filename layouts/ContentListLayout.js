0738c4fde927158cde478aff05b6dd2c2958e354
김문진 <gimmunjin@gimmunjin-ui-MacBookAir.local>
2023-03-21 16:28:38 +0900

    moonjinkim, Feat : 블로그 검색 조건 기능 추가

--- layouts/ContentListLayout.js
+++ layouts/ContentListLayout.js
@@ -21,6 +21,7 @@ import {
   SecContentTitleAndDesc,
   SubSecInnerIntroTitle,
   SubSecInnerIntroDesc,
+  BlogSearchTagBar,
 } from "@/styles/layoutStyles/ContentListLayoutStyle";
 import React, { useState } from "react";
 
@@ -33,10 +34,27 @@ export default function ContentListLayout({
   description,
 }) {
   const [searchValue, setSearchValue] = useState("");
+  const [searchOptionValue, setSearchOptionValue] = useState("1");
+
+  const handleSelect = (e) => {
+    setSearchOptionValue(e.target.value);
+  }
+
+  const filterOptionBlogPosts = postData => {
+    if (searchOptionValue === "1"){
+      return postData.title + postData.summary + postData.tags.join(" ");
+    } else if (searchOptionValue === "2" ) {
+      return postData.authors.join(" ");
+    } else if ( searchOptionValue === "3") {
+      return postData.title;
+    }else {
+      return postData.tags.join(" ")
+    }
+  }
 
   const filteredBlogPosts = posts.filter((postData) => {
-    const searchContent =
-      postData.title + postData.summary + postData.tags.join(" ");
+    const searchContent = filterOptionBlogPosts(postData);
+    console.log(searchContent)
     return searchContent.toLowerCase().includes(searchValue.toLowerCase());
   });
 
@@ -59,6 +77,12 @@ export default function ContentListLayout({
       <SubSecContent>
         <SubSecInner>
           <SubSecSearchBar>
+            <BlogSearchTagBar onChange={handleSelect} value={searchOptionValue}>
+              <option value="1" >제목+내용</option>
+              <option value="2">작성자</option>
+              <option value="3">제목</option>
+              <option value="4">내용</option>
+            </BlogSearchTagBar>
             <BlogSearchBar
               type="text"
               onChange={(e) => setSearchValue(e.target.value)}
